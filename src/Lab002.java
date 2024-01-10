930b7989d256235017562838f8c9f9153f521287
Keerthy Krishnan <keerthy310595@gmail.com>
2024-01-09 20:06:04 -0600

    pushing Lab002 and Lab003

---
+++ src/Lab002.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        // range of byte = -128 to 127
+        byte age = 10;
+      //  age = 129;  // incompatible becoz its out of range
+        age = 127;
+        System.out.println(age);
+        System.out.println(age+5);
+        System.out.println(age-10);
+
+    }
+}
---
+++ src/Lab003.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Lab003 {
+    public static void main(String[] args) {
+
+        // short range starts from -32768 to 32767
+        short age = 130;
+        // short age 2 = 400000000; (range exceeds)
+        System.out.println(age+20);
+
+
+    }
+}
