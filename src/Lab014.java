2a68b700fb32d6ca4eb8bd1bafccff6d6921ed61
Keerthy Krishnan <keerthy310595@gmail.com>
2024-01-13 00:20:26 -0600

    pushing Lab004 to Lab013

---
+++ src/Lab014.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Lab014 {
+    public static void main(String[] args) {
+
+        // 4 operations of math
+        int a = 20;
+        int b = 30;
+        System.out.println(a+b);
+
+        byte c = 5;
+        int d = 7;
+        System.out.println(c*d);
+
+        float e = 4.5f;
+        long f = 95L;
+        System.out.println(e-f);
+
+        int g = 456;
+        byte h = 24;
+        System.out.println(g/h);
+
+    }
+}
