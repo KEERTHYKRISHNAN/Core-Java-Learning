d12a8f2ff4cf8a1e1def9bb2167bb969968de00e
Keerthy Krishnan <keerthy310595@gmail.com>
2024-02-13 17:35:39 -0600

    pushing Labs

---
+++ OOPSConcept/Lab147Person.java
@@ -0,0 +1,31 @@
+package OOPSConcept;
+
+public class Lab147Person {
+    public static void main(String[] args) {
+
+
+        //Person -- class blueprint - method/function
+        //p1 p2 p3 -- ref variable
+        //p1 points to keerthy object
+       // new person - Real object
+
+
+        Person p = new Person();
+        p.name = "Keerthy";
+        System.out.println(p.name);
+
+        p.age = 27;
+        System.out.println(p.age);
+
+        p.email = "keerthy31@gmail.com";
+        System.out.println(p.email);
+
+        Person p2 =  new Person();
+        p2.name = "Keerthy";
+        System.out.println(p2.name);
+
+        new Person();
+        Person p3 = null;
+
+    }
+}
---
+++ OOPSConcept/Person.java
@@ -0,0 +1,24 @@
+package OOPSConcept;
+
+public class Person {
+    //Attribute
+    String name;
+    int age;
+    String email;
+    Long phonenumber;
+    Double heightweight;
+    String address;
+
+
+
+    //Behaviour
+    void walk (){
+        System.out.println("walk");
+    }
+
+    void sleep () {
+        System.out.println("sleep");
+    }
+
+    boolean result = true;
+}
